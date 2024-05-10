18a32e0e2deec17461064e353534574b28fff080
Zulaikha <zhanif02@gmail.com>
2024-05-10 18:52:11 +0100

    booksinfo pg done

--- src/Pages/Bookinfo.jsx
+++ src/Pages/Bookinfo.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Link, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import Rating from "../Components/ui/Rating";
 import Price from "../Components/ui/Price";
+import Book from "../Components/ui/Book";
 
 const Bookinfo = ({ books }) => {
   const {id} = useParams();
@@ -23,25 +24,19 @@ const Bookinfo = ({ books }) => {
             <div className="book__selected">
               <figure className="book__selected--figure">
                 <img
-                  src="https://covers.openlibrary.org/b/id/10958382-L.jpg"
+                  src={book.url}
                   alt=""
                   className="book__selected--img"
                 />
               </figure>
               <div className="book__selected--description">
-                <h2 className="book__selected--title">Atomic Habits</h2>
-                <Rating rating="4.5" />
+                <h2 className="book__selected--title">{book.title}</h2>
+                <Rating rating={book.rating} />
                 <div className="book__selected--price">
-                  <Price originalPrice={12} salePrice={8} />
+                  <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
                 </div>
                 <div className="book__summary">
-                  <div className="book__summary--title">Summary</div>
-                  <p className="book__summary--para">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Voluptatibus ipsum aliquid soluta enim, molestiae nesciunt
-                    ut eaque est odit explicabo ipsa laborum quod placeat
-                    recusandae itaque architecto omnis, eligendi veniam.
-                  </p>
+                  <h3 className="book__summary--title">Summary</h3>
                   <p className="book__summary--para">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                     Voluptatibus ipsum aliquid soluta enim, molestiae nesciunt
@@ -70,7 +65,14 @@ const Bookinfo = ({ books }) => {
                 Recommended Books
               </h2>
             </div>
-
+            <div className="books">
+            {books
+            .filter((book) => book.rating === 5 && +book.id !== +id)
+            .slice(0,4)
+            .map(book => <Book book={book} key={book.id} />)
+          }
+            </div>
+            
           </div>
         </div>
         
