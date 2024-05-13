f847e2a7605cad89aa3e60daca7abe0be3262b08
Zulaikha <zhanif02@gmail.com>
2024-05-13 19:02:21 +0100

    sorted out cart quantities

--- src/App.js
+++ src/App.js
@@ -12,7 +12,25 @@ function App() {
   const [cart, setCart] = useState([]);
 
   function addToCart(book) {
-    setCart([...cart, book])
+    const dupeItem = cart.find(item => +item.id === +book.id)
+    if (dupeItem){
+      setCart(cart.map(item => {
+         if (item.id === dupeItem.id){
+          return {
+            ...item,
+            quantity: item.quantity + 1,
+          }
+          
+         }
+         else{
+          return item
+         }
+      }))
+    }
+    else {
+      setCart([...cart, {...book, quantity: 1}])
+
+    }
   }
 
 useEffect(() => {
